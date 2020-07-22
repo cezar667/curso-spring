f924ede37eac18a2c41b22e9ec0852d768fc5fe1
Cezar Augusto <cezar667@gmail.com>
2020-07-22 11:09:19 -0300

    Projeto no vscode

--- aprendendospringvs/demo/src/main/java/com/cursospringvs/demo/DemoApplication.java
+++ aprendendospringvs/demo/src/main/java/com/cursospringvs/demo/DemoApplication.java
@@ -1,14 +1,13 @@
-package com.cursos.aprendendospring;
+package com.cursospringvs.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AprendendoSpringApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(AprendendoSpringApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
